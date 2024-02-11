ddcc7950bcb842a08e42c91875f45319373ef041
Olivia Golden <olivia.golden.w@gmail.com>
2024-02-10 20:38:16 -0600

    initial companies routes

--- routes/companies.js
+++ routes/companies.js
@@ -15,8 +15,7 @@ router.get("/", async function (req, res, next) {
 
 router.get("/:code", async function (req, res, next) {
     try {
-        console.log(req.params.code);
-        const results = await db.query(`SELECT * FROM companies WHERE code = '${req.params.code}'`);
+        const results = await db.query(`SELECT * FROM companies WHERE code = $1`, [req.params.code]);
         return res.json({ company: results.rows });
     }
     catch (err) {
@@ -29,6 +28,28 @@ router.post("/", async function (req, res, next) {
         const { code, name, description } = req.body;
         const results = await db.query(`INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING code, name, description`, [code, name, description]);
         return res.json({ companies: results.rows });
+    }
+    catch (err) {
+        return next(err);
+    }
+})
+
+router.put("/:code", async function (req, res, next) {
+    try {
+        const code = req.params.code;
+        const { name, description } = req.body;
+        const results = await db.query(`UPDATE companies SET name = $1, description = $2 WHERE code = $3 RETURNING code, name, description`, [name, description, code]);
+        return res.json({ company: results.rows });
+    }
+    catch (err) {
+        return next(err);
+    }
+})
+
+router.delete("/:code", async function (req, res, next) {
+    try {
+        await db.query(`DELETE FROM companies WHERE code = $1`, [req.params.code]);
+        return res.json({ status: 'deleted'});
     }
     catch (err) {
         return next(err);
